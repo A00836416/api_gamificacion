e75b6c0ce84b2a7410962f03651777ef365aaa0e
Harold <A00836416@tec.mx>
2024-09-02 22:55:16 -0600

    CORS Fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/auth.js';
 import { authMiddleware, isAdmin } from './middlewares/auth.js';
@@ -12,6 +13,13 @@ const app = express();
 
 await connectDB();
 
+// Configuración de CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Asegúrate de que esta URL coincida con la del frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // Habilita el envío de cookies y encabezados de autorización
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,6 +29,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000 // 30 días
   }
 }));
@@ -35,6 +44,10 @@ app.get('/admin', authMiddleware, isAdmin, (req, res) => {
   res.json({ message: 'Esta es una ruta solo para administradores' });
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/auth/check-auth', authMiddleware, (req, res) => {
+  res.json({ isAuthenticated: true, user: req.user });
+});
+
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
